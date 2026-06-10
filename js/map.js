97c125a8b6da9752a426de06404ade0edbbf978e
tedypriadi <tedypriadi.kkp@gmail.com>
2026-06-10 23:26:39 +0700

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,1 +1,27 @@
+// Membuat peta Indonesia
 
+var map = L.map('map').setView([-2.5,118],5);
+
+L.tileLayer(
+'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png',
+{
+    attribution:''
+}
+).addTo(map);
+
+// Membaca GeoJSON
+
+fetch('data/provinsi.geojson')
+.then(response => response.json())
+.then(data => {
+
+    L.geoJSON(data,{
+        style:{
+            color:'#ffffff',
+            weight:1,
+            fillColor:'#009688',
+            fillOpacity:0.8
+        }
+    }).addTo(map);
+
+});
