1c8476ca6196da24ef4bb4e9b537f1a827097611
tedypriadi <tedypriadi.kkp@gmail.com>
2026-06-11 00:04:10 +0700

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,71 +1,65 @@
-var map = L.map('map').setView([-2.5,118],5);
+var map = L.map('map').setView([-2.5, 118], 5);
 
 L.tileLayer(
-'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png',
-{
-    attribution:''
-}
+  'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png',
+  {
+    attribution: ''
+  }
 ).addTo(map);
 
 fetch('data/status.json')
-.then(response => response.json())
-.then(statusData => {
+  .then(response => response.json())
+  .then(statusData => {
 
     fetch('data/provinsi.geojson')
-    .then(response => response.json())
-    .then(geojsonData => {
+      .then(response => response.json())
+      .then(geojsonData => {
 
-        function getColor(status){
+        function getColor(status) {
 
-            if(status === "Terintegrasi")
-                return "#00B894";
+          if (status === "Terintegrasi")
+            return "#00B894";
 
-            if(status === "Proses Integrasi")
-                return "#FDCB6E";
+          if (status === "Proses Integrasi")
+            return "#FDCB6E";
 
-            if(status === "Penyusunan Materi Teknis")
-                return "#E17055";
+          if (status === "Penyusunan Materi Teknis")
+            return "#E17055";
 
-            if(status === "Tidak Memiliki Wilayah Laut")
-                return "#636E72";
+          if (status === "Tidak Memiliki Wilayah Laut")
+            return "#636E72";
 
-            return "#CCCCCC";
+          return "#CCCCCC";
         }
 
         L.geoJSON(geojsonData, {
 
-            style: function(feature){
+          style: function(feature) {
 
-                let provinsi =
-                    feature.properties.WADMPR;
+            let provinsi = feature.properties.WADMPR;
+            let status = statusData[provinsi];
 
-                let status =
-                    statusData[provinsi];
+            return {
+              color: "#FFFFFF",
+              weight: 1,
+              fillColor: getColor(status),
+              fillOpacity: 0.8
+            };
+          },
 
-                return {
-                    color:"#FFFFFF",
-                    weight:1,
-                    fillColor:getColor(status),
-                    fillOpacity:0.8
-                };
-            },
+          onEachFeature: function(feature, layer) {
 
-            onEachFeature: function(feature, layer){
+            let provinsi = feature.properties.WADMPR;
+            let status = statusData[provinsi] || "Belum ada data";
 
-                let provinsi =
-                    feature.properties.WADMPR;
+            layer.bindPopup(
+              "<b>" + provinsi + "</b><br>Status: " + status
+            );
 
-                let status =
-                    statusData[provinsi] || "Belum ada data";
-
-                layer.bindPopup(
-                    "<b>" + provinsi + "</b><br>" +
-                    "Status: " + status
-                );
-            }
+          }
 
         }).addTo(map);
 
-    });
+      });
 
-});
+  });
