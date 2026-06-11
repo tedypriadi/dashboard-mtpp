a0e53ac2239c4286b0c65ecdf2870be7e1778725
tedypriadi <tedypriadi.kkp@gmail.com>
2026-06-11 12:29:35 +0700

    Update chart.js

--- js/chart.js
+++ js/chart.js
@@ -2,19 +2,64 @@ const ctx = document.getElementById('perdaChart');
 
 new Chart(ctx, {
     type: 'bar',
+
     data: {
         labels: ['2022','2023','2024','2025'],
         datasets: [{
             label: 'Jumlah Perda',
-            data: [2,10,10,3]
+            data: [2,10,10,3],
+            backgroundColor: '#00B894'
         }]
     },
+
     options: {
-        responsive:true,
-        plugins:{
-            legend:{
-                display:false
+
+        responsive: true,
+
+        plugins: {
+
+            legend: {
+                display: false
+            },
+
+            title: {
+                display: true,
+                text: 'Penetapan Perda RTRW Terintegrasi',
+                color: 'white'
             }
+
+        },
+
+        scales: {
+
+            x: {
+
+                ticks: {
+                    color: 'white'
+                },
+
+                grid: {
+                    color: 'rgba(255,255,255,0.1)'
+                }
+
+            },
+
+            y: {
+
+                beginAtZero: true,
+
+                ticks: {
+                    color: 'white'
+                },
+
+                grid: {
+                    color: 'rgba(255,255,255,0.1)'
+                }
+
+            }
+
         }
+
     }
+
 });
