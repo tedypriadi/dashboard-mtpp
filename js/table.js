99de2bc5ab3d52c7ed62904004dd0e5af02c02d8
tedypriadi <tedypriadi.kkp@gmail.com>
2026-06-11 12:48:03 +0700

    Create table.js

---
+++ js/table.js
@@ -0,0 +1,41 @@
+fetch('data/provinsi.json')
+.then(response => response.json())
+.then(data => {
+
+    const tbody =
+        document.querySelector('#provinsi-table tbody');
+
+    tbody.innerHTML = '';
+
+    data.forEach((item,index)=>{
+
+        tbody.innerHTML += `
+        <tr>
+
+            <td>${index + 1}</td>
+
+            <td>${item.provinsi}</td>
+
+            <td>${item.statusIntegrasi}</td>
+
+            <td>
+                ${
+                    item.linkPerda
+                    ?
+                    `<a href="${item.linkPerda}" target="_blank">
+                        ${item.nomorPerda}
+                    </a>`
+                    :
+                    item.nomorPerda
+                }
+            </td>
+
+            <td>${item.statusRZWP3K}</td>
+
+            <td>${item.riwayat}</td>
+
+        </tr>
+        `;
+    });
+
+});
